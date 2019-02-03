6845f6ee2d27c5052ee0a0c8233669752723b64c
Omar Ihmoda <omar.ihmoda@gmail.com>
2019-02-03 08:14:40 -0800

    refactor polling using rxjs, add start/stop polling functionality

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
 import React, { Component } from "react";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { Subject, from, timer } from "rxjs";
+import { map, switchMap, takeUntil, filter } from "rxjs/operators";
 import logo from "./logo.svg";
 import "./App.css";
+
+const apiUrl = "https://pokeapi.co/api/v2/pokemon";
+const STOP = "STOP";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    const subject = new Subject();
+    const endObservable$ = subject.pipe(filter(val => val === STOP));
+
     this.state = {
       name: "Bulbasaur",
       imgUrl:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
-      number: 1
+      number: 1,
+      endObservable$
     };
-
-    const $observer = new Observable(observer => {
-      setInterval(() => {
-        const pokemonNumber =
-          this.state.number < 152 ? this.state.number + 1 : 1;
-        this.setState({ number: pokemonNumber });
-        const result = fetch(
-          `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`
-        )
-          .then(result => result.json())
-          .then(data => observer.next(JSON.parse(JSON.stringify(data))));
-      }, 3000);
-    });
-
-    $observer
-      .pipe(
-        map(data => {
-          return {
-            name: `${data.name[0].toUpperCase()}${data.name.substring(1)}`,
-            imgUrl: data.sprites.back_default
-          };
-        })
-      )
-      .subscribe(this._getData);
   }
 
-  _getData = data => {
-    this.setState(() => ({
-      name: data.name,
-      imgUrl: data.imgUrl
-    }));
+  _updatePokemon = data => {
+    const { name, imgUrl } = data;
+    this.setState({
+      name,
+      imgUrl
+    });
+  };
+
+  _requestData(url) {
+    return from(fetch(url)).pipe(
+      switchMap(data => data.json()),
+      map(data => {
+        return {
+          name: `${data.name[0].toUpperCase()}${data.name.substring(1)}`,
+          imgUrl: data.sprites.back_default
+        };
+      })
+    );
+  }
+
+  _startPolling = (url, interval = 5000) => {
+    const { endObservable$ } = this.state;
+    return timer(1, interval)
+      .pipe(
+        switchMap(x => {
+          const fullUrl = `${url}/${x + 1}/`;
+          return this._requestData(fullUrl);
+        }),
+        takeUntil(endObservable$)
+      )
+      .subscribe(this._updatePokemon);
+  };
+
+  _stopPolling = () => {
+    const { endObservable$ } = this.state;
+    endObservable$.next(STOP);
   };
 
   render() {
@@ -52,6 +66,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Pokemon Cycler</h1>
+        <button onClick={() => this._startPolling(apiUrl)}>
+          Start Polling
+        </button>
+        <button onClick={() => this._stopPolling(apiUrl)}>Stop Polling</button>
         <h1>{name}</h1>
         <img src={imgUrl} />
       </div>
